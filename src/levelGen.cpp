bc89c31654bd8cb0d8f68270bdc5dffdb814c338
Martijn van Iersel <mvaniersel@gmail.com>
2020-05-09 16:57:55 +0200

    Doors and keys

--- src/levelGen.cpp
+++ src/levelGen.cpp
@@ -54,13 +54,21 @@ T pop(std::vector<T> &aContainer) {
 
 struct Node {
 	int area;
+	int doorArea;
 	int x, y;
 	map<Dir, Node*> links;
+	map<Dir, bool> locks;
+
 	char letter; // for teleporter edges...
 	bool hasLink(Dir dir) {
 		return hasKey(links, dir);
 	}
-	
+	bool hasLock(Dir dir) {
+		return hasKey(locks, dir);
+	}
+	int pStart = 0;
+	bool hasBanana = false;
+	bool hasKeycard = false;
 };
 
 class Edge {
@@ -118,11 +126,21 @@ string toString(const Map2D<Cell> &map) {
 			}
 			else {
 				auto &node = cell.nodes[0];
-				row[0] << (node.hasLink(N) ? "▒▒ ▒": "▒▒▒▒");
-				// row[1] << string_format ("▒%02i ", node.area);
-				row[1] << "▒   ";
-				row[2] << (node.hasLink(W) ? "    " : "▒   ");
-				row[3] << (string_format("▒  %c", node.hasLink(TELEPORT) ? node.letter : ' '));
+				
+				char pstartChar = node.pStart == 0 ? ' ' : (node.pStart == 1 ? '1' : '2');
+				const char *northDoor = (node.hasLink(N) ? (node.hasLock(N) ? "X" : " ") : "▒");
+				const char *westDoor = (node.hasLink(W) ? (node.hasLock(W) ? "X" : " ") : "▒");
+				
+
+				row[0] << string_format("▒▒%s▒", northDoor);
+				row[1] << string_format ("▒%02i ", node.doorArea);
+				// row[1] << "▒   ";
+				row[2] << string_format("%s %c ", westDoor, pstartChar);
+				row[3] << string_format("▒%c%c%c", 
+					node.hasBanana ? 'b' : ' ', 
+					node.hasKeycard ? 'k' : ' ', 
+					node.hasLink(TELEPORT) ? node.letter : ' '
+				);
 			}
 		}
 		result << row[0].str() << "▒\n" << row[1].str() << "▒\n" << row[2].str() << "▒\n" << row[3].str() << "▒\n";
@@ -178,27 +196,6 @@ vector<Edge> listEdges(Map2D<Cell> &lvl, Node *node) {
 }
 
 
-void linkNodes(Node *src, Node *dest, Dir dir, bool reverse = true) {
-	assert(!hasKey(src->links, dir));
-	src->links[dir] = dest;
-
-	if (reverse) {
-		linkNodes(dest, src, DIR[dir].reverse, false);
-	}
-}
-
-void convertArea(Map2D<Cell> &lvl, int srcArea, int destArea) {
-	for (size_t y = 0; y < lvl.getDimMY(); ++y) {
-		for (size_t x = 0; x < lvl.getDimMY(); ++x) {
-			if (lvl.get(x, y).isEmpty()) continue;
-			auto &node = lvl.get(x, y).nodes[0];
-			if (node.area == srcArea) {
-				node.area = destArea;
-			}
-		}
-	}
-};
-
 vector<Node*> getAllNodes(Map2D<Cell> &lvl) {
 	vector<Node*> result;
 	for (size_t y = 0; y < lvl.getDimMY(); ++y) {
@@ -210,6 +207,96 @@ vector<Node*> getAllNodes(Map2D<Cell> &lvl) {
 	return result;
 }
 
+vector<Node*> getDoorAreaNodes(Map2D<Cell> &lvl, int area) {
+	vector<Node*> result;
+	for (size_t y = 0; y < lvl.getDimMY(); ++y) {
+		for (size_t x = 0; x < lvl.getDimMY(); ++x) {
+			if (lvl.get(x, y).isEmpty()) continue;
+			Node *n = &lvl.get(x, y).nodes[0];
+			if (n->doorArea == area) {
+				result.push_back(n);
+			}
+		}
+	}
+	return result;
+}
+
+const int NUM_BANANAS = 3;
+
+void placeObjects(Map2D<Cell> &lvl, int area1, int area2) {
+	auto allNodes = getAllNodes(lvl);
+
+	auto area1Nodes = getDoorAreaNodes(lvl, area1);
+	auto area2Nodes = getDoorAreaNodes(lvl, area2);
+
+	if (area1Nodes.size() < area2Nodes.size()) {
+		auto temp = area1Nodes;
+		area1Nodes = area2Nodes;
+		area2Nodes = temp;
+	}
+
+	random_shuffle(area1Nodes.begin(), area1Nodes.end());
+	random_shuffle(area2Nodes.begin(), area2Nodes.end());
+	random_shuffle(allNodes.begin(), allNodes.end());
+	
+	// area1 is the largest area.
+	// put the players in there.
+	auto n1 = pop(area1Nodes);
+	n1->pStart = 1;
+	auto n2 = pop(area1Nodes);
+	n2->pStart = 2;
+
+	// put the key in area1
+	auto n3 = pop(area1Nodes);
+	n3->hasKeycard = true;
+	
+	int bananaRemain = NUM_BANANAS;
+	// put at least one banana behind the door
+	auto n4 = pop(area2Nodes);
+	n4->hasBanana = true;
+	bananaRemain--;
+
+	while (bananaRemain > 0) {
+		auto b = pop(allNodes);
+		if (!b->hasBanana) {
+			b->hasBanana = true;
+			bananaRemain--;
+		}
+	}
+
+	// start over
+	allNodes = getAllNodes(lvl);
+	
+}
+
+void linkNodes(Node *src, Node *dest, Dir dir, bool reverse = true, bool doorLink = false) {
+	assert(!hasKey(src->links, dir));
+	src->links[dir] = dest;
+
+	if (doorLink) {
+		src->locks[dir] = true;
+	}
+
+	if (reverse) {
+		linkNodes(dest, src, DIR[dir].reverse, false, doorLink);
+	}
+}
+
+void convertArea(Map2D<Cell> &lvl, int srcArea, int destArea, bool convertDoors=true) {
+	for (size_t y = 0; y < lvl.getDimMY(); ++y) {
+		for (size_t x = 0; x < lvl.getDimMY(); ++x) {
+			if (lvl.get(x, y).isEmpty()) continue;
+			auto &node = lvl.get(x, y).nodes[0];
+			if (node.area == srcArea) {
+				node.area = destArea;
+				if (convertDoors) {
+					node.doorArea = destArea;
+				}
+			}
+		}
+	}
+};
+
 void kruskal(Map2D<Cell> &lvl) {
 	vector<Edge> allEdges;
 	
@@ -218,6 +305,7 @@ void kruskal(Map2D<Cell> &lvl) {
 	// list all possible edges
 	for (auto n : getAllNodes(lvl)) {
 		n->area = areaCounter++;
+		n->doorArea = n->area;
 		for (auto &edge : listEdges(lvl, n)) {
 			allEdges.push_back(edge);
 		}
@@ -237,10 +325,15 @@ void kruskal(Map2D<Cell> &lvl) {
 	random_shuffle(allEdges.begin(), allEdges.end());
 
 	int countAreas = areaCounter;
-	while (allEdges.size() > 0 && countAreas > 1) {
+	while (allEdges.size() > 0 && areaCounter > 1) {
 		// pick a random edge
 		// does it link two areas?
 		Edge e = pop(allEdges);
+
+		// if (countAreas == 2 && e.dir == TELEPORT) {
+		// 	continue;
+		// 	//TODO! there is a risk that there is nothing left...
+		// }
 
 		if (e.src->area == e.dest->area) {
 			// this is now a candidate for creating a loop
@@ -249,12 +342,21 @@ void kruskal(Map2D<Cell> &lvl) {
 			cout << "Adding link: " << e.toString() << endl;
 			// create an edge, update areas
 			// until there is only one area
-			linkNodes(e.src, e.dest, e.dir);
+			
+			if (areaCounter == 2 && e.dir != TELEPORT) {
+				linkNodes(e.src, e.dest, e.dir, true, true);
+				convertArea(lvl, e.dest->area, e.src->area, false);
+
+				placeObjects(lvl, e.src->doorArea, e.dest->doorArea);
+			}
+			else {
+				linkNodes(e.src, e.dest, e.dir);
+				convertArea(lvl, e.dest->area, e.src->area);
+			}
 			if (e.dir == TELEPORT) {
 				e.src->letter = e.letter;
 				e.dest->letter = e.letter;
 			}
-			convertArea(lvl, e.dest->area, e.src->area);
 			areaCounter--;
 
 			// print intermediate step for debugging
